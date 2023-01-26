0a002d2c8a2f292b7679a51335929f9849f6d29e
rayan1159 <baskruithof999@gmail.com>
2023-01-26 13:02:17 +0100

    Added get_response_as_string to chat_sonic.py

--- PyAPI/endpoints/chat_sonic.py
+++ PyAPI/endpoints/chat_sonic.py
@@ -1,78 +1,79 @@
-from PyAPI.http import request
+import json
+
+from ..http import request
 import requests
 
 
 class ChatSonic:
     token = ""
+    enable_memory = False
+    input_text = None
+    enable_google_results = False
 
-    def  __init__(self, token):
+    def  __init__(self, token, input_text, enable_memory, enable_google_results):
         """
         :param token:
         :return: None
         """
 
         self.token = token
+        self.input_text = input_text
+        self.enable_memory = enable_memory
+        self.enable_google_results = enable_google_results
         pass
 
-    def get_response_as_string(self, enable_memory=False, input_text=None, enable_google_results=None):
+    def get_response_as_string(self) -> str:
         """
         Gets the response from get_response and turns message into a string
 
-        :param enable_memory:
-        :param input_text:
-        :param enable_google_results:
-        :return: None
-
+        :return: str
         :exception requests.exceptions.HTTPError, requests.exceptions.ConnectionError, TypeError
         """
 
-    def get_response_as_array(self, enable_memory=False, input_text=None, enable_google_results=None):
+        response = self.get_response()
+        string = json.dumps(response)
+
+        return string
+
+    def get_response_as_array(self) -> []:
         """
         Gets the response from get_response and turns message into an array
 
-        :param enable_memory:
-        :param input_text:
-        :param enable_google_results:
         :return: []
-
         :exception requests.exceptions.HTTPError, requests.exceptions.ConnectionError, TypeError
         """
 
-        response = self.get_response(enable_memory=enable_memory, input_text=input_text,
-                                     enable_google_results=enable_google_results)
+        response = self.get_response()
 
         word_array = []
 
         message = response["message"]
-        for word in message.split():
+        for word in message.split(" "):
             word_array.append(word)
         pass
 
         return word_array
 
-    def get_response(self, enable_memory, input_text, enable_google_results):
+    def get_response(self):
         """
         Get a response from the chatsonic endpoint
 
-        :param enable_memory:
-        :param input_text:
-        :param enable_google_results:
         :return:
 
         :exception requests.exceptions.HTTPError, requests.exceptions.ConnectionError, TypeError
         """
         is_token_string = type(self.token) is str
-        is_enable_mem = type(enable_memory) is bool
-        is_input_string = type(input_text) is str
-        is_google_results = type(enable_google_results) is bool
+        is_enable_mem = type(self.enable_memory) is bool
+        is_input_string = type(self.input_text) is str
+        is_google_results = type(self.enable_google_results) is bool
 
         if is_token_string and is_enable_mem and is_input_string and is_google_results:
             req = request.Request()
             try:
                 json_object = req.post("https://api.writesonic.com/v2/business/content/chatsonic?engine=premium", data={
-                    "enable_google_results": enable_google_results,
-                    "enable_memory": enable_memory,
-                    "input_text": input_text
+                    "enable_google_results": self.enable_google_results,
+                    "enable_memory": self.enable_memory,
+                    "input_text": self.input_text
                 }, headers={
                     "accept": "application/json",
                     "content-type": "application/json",
--- wrapper-test.py
+++ wrapper-test.py
@@ -8,10 +8,10 @@ if __name__ == "__main__":
     And is only meant to be used for command line testing.  
     
     """
-    chat_sonic = ChatSonic.ChatSonic()
+    chat_sonic = ChatSonic.ChatSonic(token="1a39b2fb-bd07-4508-b0b1-142c32e5daec", enable_memory=True, input_text="Hello", enable_google_results=True)
 
     try:
-        print(chat_sonic.get_response(enable_memory=True, input_text="Hello", enable_google_results=True))
+        print(chat_sonic.get_response_as_array())
     except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError, TypeError) as e:
         print(e)
 pass
